4be8d02a4f1de090085c8d6c78e7446ec0383427
feldmann.37 <feldmann.37@osu.edu>
2017-10-10 18:08:09 -0400

    Broken permutator

--- Solver/Program.cs
+++ Solver/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Solver
 {
     class Program
@@ -7,6 +7,40 @@ namespace Solver
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<int> tList = new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            List<List<int>> toPrint = ListPermutations(tList);
+            foreach(List<int> perm in toPrint){
+                Console.Write(perm);
+            }
         }
+
+        //TODO Checker
+
+        //TODO Permutator
+        private static List<List<int>> ListPermutations(List<int> l){
+            List<List<int>> permList = new List<List<int>>();
+            int s = l.Count;
+            if (s <= 1){
+                permList.Add(l);
+                return permList;
+            }
+
+            for (int i = 0; i < s; i++){
+                int x = l[i];
+                l.RemoveAt(i);
+                List<List<int>> temp = ListPermutations(l); 
+                foreach (List<int> y in temp){
+                    y.Insert(0, x);
+                    permList.Add(y);
+                }
+                l.Insert(i, x);
+            }
+            return permList;
+        }
+
+
+        //TODO Initializer
+
+        
     }
 }
