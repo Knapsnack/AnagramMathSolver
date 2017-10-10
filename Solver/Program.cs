14e7fbd0e93cd9bb1d13f05a284f8c13c275ac20
feldmann.37 <feldmann.37@osu.edu>
2017-10-10 19:08:45 -0400

    Working Permutator

--- Solver/Program.cs
+++ Solver/Program.cs
@@ -8,17 +8,29 @@ namespace Solver
         {
             Console.WriteLine("Hello World!");
             List<int> tList = new List<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            List<List<int>> toPrint = ListPermutations(tList);
-            foreach(List<int> perm in toPrint){
-                Console.Write(perm);
+            for(int i = 0; i < tList.Count; i++){
+                Console.Write(tList[i]);
             }
+            try {
+                List<List<int>> toPrint = ListPermutations(tList);
+                foreach(List<int> perm in toPrint){
+                    for(int i = 0; i < perm.Count; i++){
+                        Console.Write(perm[i]);
+                    }        
+                    Console.WriteLine();
+                }
+            } catch {
+                Console.Write("I Goofed");
+            }
+            
         }
 
         //TODO Checker
 
         //TODO Permutator
-        private static List<List<int>> ListPermutations(List<int> l){
+        private static List<List<int>> ListPermutations(List<int> values){
             List<List<int>> permList = new List<List<int>>();
+            List<int> l = new List<int>(values);
             int s = l.Count;
             if (s <= 1){
                 permList.Add(l);
@@ -28,6 +40,12 @@ namespace Solver
             for (int i = 0; i < s; i++){
                 int x = l[i];
                 l.RemoveAt(i);
+
+                //Console.Write(l);
+                //for(int j = 0; j < l.Count; j++){
+                //    Console.Write(l[j]);
+                //}
+
                 List<List<int>> temp = ListPermutations(l); 
                 foreach (List<int> y in temp){
                     y.Insert(0, x);
